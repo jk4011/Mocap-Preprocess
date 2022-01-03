a0ef2b8bedcb4a6b3973448d7988502f3634917b
jk4011 <wlsgur4011@unist.ac.kr>
2022-01-03 23:04:21 +0900

    def save() done

--- bvh_converter/dme2lafan/bvh_converter.py
+++ bvh_converter/dme2lafan/bvh_converter.py
@@ -1,40 +1,94 @@
-import re, os, ntpath
 import numpy as np
-from .dme2lafan import utils
+import re, os, ntpath
 
 channelmap = {"Xrotation": "x", "Yrotation": "y", "Zrotation": "z"}
-
-channelmap_inv = {
-    "x": "Xrotation",
-    "y": "Yrotation",
-    "z": "Zrotation",
-}
-
-ordermap = {
-    "x": 0,
-    "y": 1,
-    "z": 2,
-}
-
 
 class Anim(object):
     """
     A very basic animation object
     """
 
-    def __init__(self, quats, pos, offsets, parents, bones):
+    def __init__(self, rotation, pos, offsets, parents, bones, meta_data):
         """
-        :param quats: local quaternions tensor
+        :param rotation: local angular coordinate
         :param pos: local positions tensor
         :param offsets: local joint offsets
         :param parents: bone hierarchy
         :param bones: bone names
         """
-        self.quats = quats
+        self.rotation = rotation
         self.pos = pos
         self.offsets = offsets
         self.parents = parents
         self.bones = bones
+        self.meta_data = meta_data
+
+    def save(self):
+        indents = {-1: -1}
+        for i, parent in enumerate(self.parents):
+            indents[i] = indents[parent] + 1
+        
+        
+        data = []
+        prev_indent = 0
+        for idx, bone in enumerate(self.bones):
+            if bone == "Hips":
+                data += [
+                    "HIERARCHY", 
+                    "ROOT Hips", 
+                    "{",
+                    "    OFFSET 0.000000 0.000000 0.000000",
+                    "    CHANNELS 6 Xposition Yposition Zposition Zrotation Yrotation Xrotation"
+                ]
+            else:
+                indent = indents[idx]
+                if prev_indent > indent:
+                    data += [
+						(prev_indent+1) * "    " + "End Site",
+						(prev_indent+1) * "    " + "{",
+						(prev_indent+1) * "    " + "	OFFSET 0.000000 0.000000 0.000000",
+						(prev_indent+1) * "    " + "}"
+                    ]
+                    for i in range(prev_indent, indent-1, -1):
+                        data += [i * "    " + "}"]
+
+                data += [
+                    indent * "    " + f"JOINT {bone}",
+                    indent * "    " + "{",
+                    indent * "    " + "    OFFSET " + " ".join([str(i) for i in self.offsets[idx]]),
+                    indent * "    " + "    CHANNELS 3 Zrotation Yrotation Xrotation"
+                ]
+                prev_indent = indent
+        
+        data += [
+            (prev_indent+1) * "    " + "End Site",
+            (prev_indent+1) * "    " + "{",
+            (prev_indent+1) * "    " + "	OFFSET 0.000000 0.000000 0.000000",
+            (prev_indent+1) * "    " + "}"
+        ]
+        for i in range(prev_indent, -1, -1):
+            data += [i * "    " + "}"]
+
+        data += ["MOTION"]
+        data += self.meta_data
+        
+        length = self.rotation.shape[0]
+        rotation = self.rotation.reshape(length, -1)
+        root_pos = self.pos.reshape(length, -1)[:, :3]
+
+
+        for i in range(length):
+            data += ["    ".join([str(j) for j in root_pos[i]]) + "    " +
+                     "    ".join([str(j) for j in rotation[i]])]
+        
+        for i in range(len(data)):
+            if data[i][-1] != "\n":
+                data[i] += "\n"
+            
+        with open('test.bvh', 'w') as f:
+            f.writelines(data)
+                    
+    def 
 
 
 def read_bvh(filename, start=None, end=None, order=None):
@@ -58,6 +112,7 @@ def read_bvh(filename, start=None, end=None, order=None):
     orients = np.array([]).reshape((0, 4))
     offsets = np.array([]).reshape((0, 3))
     parents = np.array([], dtype=int)
+    meta_data = []
 
     # Parse the  file, line by line
     for line in f:
@@ -121,6 +176,7 @@ def read_bvh(filename, start=None, end=None, order=None):
 
         fmatch = re.match("\s*Frames:\s+(\d+)", line)
         if fmatch:
+            meta_data += [line]
             if start and end:
                 fnum = (end - start) - 1
             else:
@@ -131,6 +187,7 @@ def read_bvh(filename, start=None, end=None, order=None):
 
         fmatch = re.match("\s*Frame Time:\s+([\d\.]+)", line)
         if fmatch:
+            meta_data += [line]
             frametime = float(fmatch.group(1))
             continue
 
@@ -138,7 +195,11 @@ def read_bvh(filename, start=None, end=None, order=None):
             i += 1
             continue
 
-        dmatch = line.strip().split(" ")
+        for j in range(8,0,-1):
+            if re.search(j * " ", line):
+                break
+        dmatch = line.strip().split(j * " ")
+
         if dmatch:
             data_block = np.array(list(map(float, dmatch)))
             N = len(parents)
@@ -162,8 +223,13 @@ def read_bvh(filename, start=None, end=None, order=None):
 
     f.close()
 
-    rotations = utils.euler_to_quat(np.radians(rotations), order=order)
-    rotations = utils.remove_quat_discontinuities(rotations)
+    # rotations = utils.euler_to_quat(np.radians(rotations), order=order)
+    # rotations = utils.remove_quat_discontinuities(rotations)
 
-    return Anim(rotations, positions, offsets, parents, names)
+    return Anim(rotations, positions, offsets, parents, names, meta_data)
+
+data = read_bvh("jump_n_parsed.bvh")
+
+data.save()
+
 
